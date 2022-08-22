936aa93a37c5ec12f962b7ff827595015c3e12b3
Paweł Klis <pawel.klis@ppuc.pl>
2022-08-22 09:34:09 +0200

    Add project files.

---
+++ qrPrinter/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace qrPrinter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QRPrinter qr = new QRPrinter();
+            pictureBox1.Image= qr.GenerateQR(comboBox1.Text);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            System.Drawing.Printing.PrintDocument myPrintDocument1 = new System.Drawing.Printing.PrintDocument();
+            PrintDialog myPrinDialog1 = new PrintDialog();
+            myPrintDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(myPrintDocument2_PrintPage);
+            myPrinDialog1.Document = myPrintDocument1;
+            if (myPrinDialog1.ShowDialog() == DialogResult.OK)
+            {
+                myPrintDocument1.Print();
+            }
+        }
+
+        private void myPrintDocument2_PrintPage(System.Object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Bitmap myBitmap1 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.DrawToBitmap(myBitmap1, new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height));
+            e.Graphics.DrawImage(myBitmap1, 0, 0);
+            myBitmap1.Dispose();
+        }
+    }
+}
---
+++ qrPrinter/QRPrinter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.LinkLabel;
+using ZXing.QrCode.Internal;
+using System.Drawing;
+
+namespace qrPrinter
+{
+    internal class QRPrinter
+    {
+
+        public Image GenerateQR(string text)
+        {
+            Image QrCodeWriteableBitmap;
+            ZXing.BarcodeWriter barcodeWriter = new ZXing.BarcodeWriter { Format = ZXing.BarcodeFormat.QR_CODE, Options = { Height = 500, Width = 500 } };
+            QrCodeWriteableBitmap = barcodeWriter.Write(text);
+            return QrCodeWriteableBitmap;
+        }
+
+    }
+}
